d2c55deaa156beea557c0b6bf8aa07a1aea79f6a
Lopinosaurus <83396211+Lopinosaurus@users.noreply.github.com>
2023-02-20 18:47:54 +0100

    Added First Display function

--- Computer/main.lua
+++ Computer/main.lua
@@ -1,7 +1,21 @@
 -- Modem API
 
-function Connect()
-    -- Finding modem
-    local modem = peripheral.find("modem") or error("Main.Connect : No modem found.", 0)
-    modem.open(43)
+function Display()
+    io.write("Welcome to TurtleMaster Menu.")
+    io.write("To send the turtle to special coordinates, type 1.")
+    io.write("To send the turtle on your position, type 2.")
+
+    local input = io.read()
+
+    if ("1" == input) then
+        io.write("X Coord : ")
+        local xCoord = io.read()
+        io.write("Z Coord : ")
+        local zCoord = io.read()
+
+        local tableCoord = { tonumber(xCoord), 0, tonumber(zCoord) }
+        HeadToCoord(tableCoord)
+    end
 end
+
+Display()
