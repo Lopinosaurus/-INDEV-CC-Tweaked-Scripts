ef198a3af6d4196c150d9c5038e33f569dc12349
Lopinosaurus <83396211+Lopinosaurus@users.noreply.github.com>
2023-02-20 12:12:15 +0100

    Fuck nil ref

--- MasterTurtle/turtle_toolbox.lua
+++ MasterTurtle/turtle_toolbox.lua
@@ -63,9 +63,11 @@ function InventoryLookup(item)
     -- 16 is max inv index
     for i = 1, 16, 1
     do
-        if (assert(turtle.getItemDetail(1)["name"] ~= nil and assert(turtle.getItemDetail(1)["name"] == item))) then
-            invPlaces[tableCpt] = i
-            tableCpt = tableCpt + 1
+        if (assert(turtle.getItemDetail(1) ~= nil)) then
+            if (assert(turtle.getItemDetail(1)["name"])) then
+                invPlaces[tableCpt] = i
+                tableCpt = tableCpt + 1
+            end
         end
     end
 
