9a36b1305f845bad81bc6e73f8173f7d1d9a0324
Lopinosaurus <83396211+Lopinosaurus@users.noreply.github.com>
2023-02-23 00:58:22 +0100

    Indev modem interface

--- MasterTurtle/turtlemain.lua
+++ MasterTurtle/turtlemain.lua
@@ -1,3 +1,14 @@
--- Test Script
+require "turtle_toolbox"
+
 local modem = peripheral.find("modem") or error("No modem attached !", 0)
 modem.open(0)
+
+while true
+do
+    local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+
+    -- Case message == "1"
+    if string.byte(tostring(message)) == 49 then
+        -- Test for bytes coordinates, format to be determined
+    end
+end
